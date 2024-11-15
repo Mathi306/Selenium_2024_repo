2362bd672277bbb052f501ea7a06b6bb3322c28f
premk <premk@CSK>
2024-11-15 13:35:49 +0530

    Newly Added Method

---
+++ MavenProject/src/main/java/gitHub/GitHub.java
@@ -0,0 +1,15 @@
+package gitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub {
+	
+	@Test
+	
+	public void addMessage()
+	{
+		Reporter.log("Welcome to GitHub",true);
+	}
+
+}
